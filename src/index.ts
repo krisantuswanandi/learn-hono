2c629265538137489cfd8acb94e660d1f1df1049
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2024-07-25 22:44:32 +0700

    json, param, query

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,16 @@ app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
+app.get("/hello/:user", (c) => {
+  const user = c.req.param("user");
+  const mode = c.req.query("mode");
+
+  let message = `Hello, ${user}!`;
+  if (mode === "shout") {
+    message = message.toUpperCase();
+  }
+
+  return c.json({ message });
+});
+
 export default app;
